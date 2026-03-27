a2ae0fc17eaededbf7d8c3bcdbd41d2fd86ba2aa
rmkim4@gmail.com <rmkim4@gmail.com>
2026-03-27 16:29:35 +0900

    fix : rename package name

--- product-service/src/main/java/com/winner/client/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/winner/client/productservice/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package com.winner.client.productService;
+package com.winner.client.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- product-service/src/test/java/com/winner/client/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/winner/client/productservice/ProductServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.winner.client.productService;
+package com.winner.client.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
