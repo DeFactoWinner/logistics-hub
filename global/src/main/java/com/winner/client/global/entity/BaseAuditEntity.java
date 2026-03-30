11ff84a906094f868feeff9f0980419414502937
hyew0nn <hyewon-0607@naver.com>
2026-03-30 23:39:35 +0900

    feat: global 패키지 entity 관련 설정 추가

---
+++ global/src/main/java/com/winner/client/global/entity/BaseAuditEntity.java
@@ -0,0 +1,36 @@
+package com.winner.client.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseAuditEntity extends BaseTimeEntity {
+
+  @CreatedBy
+  @Column(name = "created_by", updatable = false)
+  protected UUID createdBy;
+
+  @LastModifiedBy
+  @Column(name = "updated_by")
+  private UUID updatedBy;
+
+  @Column(name = "deleted_at")
+  private LocalDateTime deletedAt;
+
+  @Column(name = "deleted_by")
+  private UUID deletedBy;
+
+  public void softDelete(UUID userId) {
+    this.deletedAt = LocalDateTime.now();
+    this.deletedBy = userId;
+  }
+}
