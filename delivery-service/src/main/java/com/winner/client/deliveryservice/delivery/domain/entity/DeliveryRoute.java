9e903114289ffe19c44286c4ea41e3824c48b5c0
hyew0nn <hyewon-0607@naver.com>
2026-03-30 12:18:48 +0900

    feat: 배송 경로 엔티티 및 배송 경로 vo(value object) 추가

---
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/domain/entity/DeliveryRoute.java
@@ -0,0 +1,90 @@
+package com.winner.client.deliveryservice.delivery.domain.entity;
+
+import com.winner.client.deliveryservice.delivery.domain.enums.DeliveryRouteStatus;
+import com.winner.client.deliveryservice.delivery.domain.vo.CurrentHubRoute;
+import com.winner.client.deliveryservice.delivery.domain.vo.Distance;
+import com.winner.client.deliveryservice.delivery.domain.vo.Duration;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(
+    name = "p_delivery_route_steps",
+    uniqueConstraints = @UniqueConstraint(
+        name = "uq_route_steps_delivery_seq",
+        columnNames = {"delivery_id", "seq"}
+    )
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DeliveryRoute {
+
+  @Id
+  @GeneratedValue
+  @UuidGenerator
+  @Column(name = "id", nullable = false, updatable = false)
+  private UUID id;
+
+  @Column(name = "delivery_id", nullable = false, updatable = false)
+  private UUID deliveryId;
+
+  @Column(name = "seq", nullable = false)
+  private int seq;
+
+  @Embedded
+  private CurrentHubRoute currentHubRoute;
+
+  @Column(name = "estimated_distance", nullable = false, precision = 10, scale = 2)
+  private Distance estimatedDistance;
+
+  @Column(name = "estimated_arrival_time", nullable = false)
+  private Duration estimatedArrivalTime;
+
+  @Column(name = "actual_distance", precision = 10, scale = 2)
+  private Distance actualDistance;
+
+  @Column(name = "actual_arrival_time")
+  private Duration actualArrivalTime;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status", nullable = false)
+  private DeliveryRouteStatus status;
+
+  @Column(name = "delivery_manager_id", nullable = false)
+  private UUID deliveryManagerId;
+
+  private static void validateSeq(int seq) {
+    if (seq < 0) {
+      throw new IllegalArgumentException("순번은 0 이상이어야 합니다.");
+    }
+  }
+
+  public DeliveryRoute(UUID deliveryId, int seq, CurrentHubRoute currentHubRoute,
+      Distance estimatedDistance, Duration estimatedArrivalTime, UUID deliveryManagerId) {
+    this.deliveryId = deliveryId;
+    this.seq = seq;
+    this.currentHubRoute = currentHubRoute;
+    this.estimatedDistance = estimatedDistance;
+    this.estimatedArrivalTime = estimatedArrivalTime;
+    this.status = DeliveryRouteStatus.WAITING;
+    this.deliveryManagerId = deliveryManagerId;
+  }
+
+  public static DeliveryRoute create(UUID deliveryId, int seq, CurrentHubRoute currentHubRoute,
+      Distance estimatedDistance, Duration estimatedArrivalTime, UUID deliveryManagerId){
+    validateSeq(seq);
+    return new DeliveryRoute(deliveryId, seq, currentHubRoute, estimatedDistance, estimatedArrivalTime, deliveryManagerId);
+  }
+}
---
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/domain/vo/CurrentHubRoute.java
@@ -0,0 +1,33 @@
+package com.winner.client.deliveryservice.delivery.domain.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CurrentHubRoute {
+  @Column(name = "cur_hub_id", nullable = false)
+  private UUID curHubId;
+
+  @Column(name = "next_hub_id", nullable = false)
+  private UUID nextHubId;
+
+  public CurrentHubRoute(UUID curHubId, UUID nextHubId) {
+    validateDifferentHubs(curHubId, nextHubId);
+    this.curHubId = curHubId;
+    this.nextHubId = nextHubId;
+  }
+
+  private static void validateDifferentHubs(UUID curHubId, UUID nextHubId) {
+    if (curHubId.equals(nextHubId)) {
+      throw new IllegalArgumentException("현재 허브와 다음 허브는 동일할 수 없습니다.");
+    }
+  }
+}
