91cea3a1e83fe212fac34d3475ec478439aa859c
sunsik17 <17sunsik@gmail.com>
2026-04-01 23:54:49 +0900

    refactor: Rename @Column mapping in DeliveryManagerUserId from delivery_manager_id to user_id

--- delivery-service/src/main/java/com/winner/client/deliveryservice/common/exception/deliverymanager/company/DeliveryManagerCompanyErrorCode.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/common/exception/deliverymanager/company/DeliveryManagerCompanyErrorCode.java
@@ -11,7 +11,9 @@ public enum DeliveryManagerCompanyErrorCode implements ErrorCode {
 	ALREADY_REGISTERED_COMPANY_DELIVERY_MANAGER("ERROR_601", HttpStatus.CONFLICT,
 		"이미 등록 된 업체 배송 담당자 입니다."),
 	EXCEEDED_TO_IN_HUB_DELIVERY_MANAGER("ERROR_602", HttpStatus.CONFLICT,
-		"허브 내에 배달담당자 TO 초과");
+		"허브 내에 배달담당자 TO 초과"),
+	NOT_FOUND_COMPANY_DELIVERY_MANAGER("ERROR_603", HttpStatus.NOT_FOUND,
+		"존재하지 않는 업체배송담당자 입니다.");
 
 	private final String code;
 	private final HttpStatus status;
