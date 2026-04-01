4d26f3964fec0bdbc2dc55873e1fc23d52b73681
hyew0nn <hyewon-0607@naver.com>
2026-04-01 23:31:44 +0900

    feat: 배송 및 배송경로 상태 전이 검증을 위해 변경 가능 상태를 Set으로 정의

--- delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/domain/enums/DeliveryRouteStatus.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/domain/enums/DeliveryRouteStatus.java
@@ -1,9 +1,47 @@
 package com.winner.client.deliveryservice.delivery.domain.enums;
 
+import java.util.Set;
+
 public enum DeliveryRouteStatus {
-  WAITING,
-  ASSIGNED,
-  IN_PROGRESS,
-  COMPLETED,
-  CANCELLED
+
+  WAITING {
+    @Override
+    public Set<DeliveryRouteStatus> nextStates() {
+      return Set.of(ASSIGNED, CANCELLED);
+    }
+  },
+
+  ASSIGNED {
+    @Override
+    public Set<DeliveryRouteStatus> nextStates() {
+      return Set.of(IN_PROGRESS, CANCELLED);
+    }
+  },
+
+  IN_PROGRESS {
+    @Override
+    public Set<DeliveryRouteStatus> nextStates() {
+      return Set.of(COMPLETED, CANCELLED);
+    }
+  },
+
+  COMPLETED {
+    @Override
+    public Set<DeliveryRouteStatus> nextStates() {
+      return Set.of();
+    }
+  },
+
+  CANCELLED {
+    @Override
+    public Set<DeliveryRouteStatus> nextStates() {
+      return Set.of();
+    }
+  };
+
+  public abstract Set<DeliveryRouteStatus> nextStates();
+
+  public boolean canTransitionTo(DeliveryRouteStatus next) {
+    return next != null && nextStates().contains(next);
+  }
 }
--- delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/domain/enums/DeliveryStatus.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/domain/enums/DeliveryStatus.java
@@ -1,12 +1,48 @@
 package com.winner.client.deliveryservice.delivery.domain.enums;
 
+import java.util.Set;
+
 public enum DeliveryStatus {
-  CREATED,
-  HUB_WAITING,
-  HUB_MOVING,
-  DESTINATION_ARRIVED,
-  FOR_VENDOR_MOVING,
-  COMPLETED,
-  CANCELLED,
-  CANCEL_REQUESTED
+
+  CREATED {
+    public Set<DeliveryStatus> nextStates() {
+      return Set.of(HUB_WAITING, CANCELLED);
+    }
+  },
+  HUB_WAITING {
+    public Set<DeliveryStatus> nextStates() {
+      return Set.of(HUB_MOVING, CANCELLED);
+    }
+  },
+  HUB_MOVING {
+    public Set<DeliveryStatus> nextStates() {
+      return Set.of(DESTINATION_ARRIVED);
+    }
+  },
+  DESTINATION_ARRIVED {
+    public Set<DeliveryStatus> nextStates() {
+      return Set.of(FOR_VENDOR_MOVING);
+    }
+  },
+  FOR_VENDOR_MOVING {
+    public Set<DeliveryStatus> nextStates() {
+      return Set.of(COMPLETED);
+    }
+  },
+  COMPLETED {
+    public Set<DeliveryStatus> nextStates() {
+      return Set.of();
+    }
+  },
+  CANCELLED {
+    public Set<DeliveryStatus> nextStates() {
+      return Set.of();
+    }
+  };
+
+  public abstract Set<DeliveryStatus> nextStates();
+
+  public boolean canTransitionTo(DeliveryStatus next) {
+    return next != null && nextStates().contains(next);
+  }
 }
