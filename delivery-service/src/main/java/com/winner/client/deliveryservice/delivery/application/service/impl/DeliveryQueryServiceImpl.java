7eca70c09b524ca1c57a473efd0540d91ab535f8
hyew0nn <hyewon-0607@naver.com>
2026-04-01 23:41:48 +0900

    style: 사용하지 않는 의존성 제거

--- delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/application/service/impl/DeliveryQueryServiceImpl.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/application/service/impl/DeliveryQueryServiceImpl.java
@@ -1,12 +1,12 @@
 package com.winner.client.deliveryservice.delivery.application.service.impl;
 
 import com.winner.client.deliveryservice.common.exception.delivery.DeliveryErrorCode;
+import com.winner.client.deliveryservice.delivery.application.service.DeliveryQueryService;
 import com.winner.client.deliveryservice.delivery.domain.entity.Delivery;
 import com.winner.client.deliveryservice.delivery.domain.entity.DeliveryRoute;
 import com.winner.client.deliveryservice.delivery.domain.repository.DeliveryRepository;
 import com.winner.client.deliveryservice.delivery.domain.repository.DeliveryRouteRepository;
 import com.winner.client.deliveryservice.delivery.presentation.dto.response.GetDeliveryResponse;
-import com.winner.client.deliveryservice.delivery.application.service.DeliveryQueryService;
 import com.winner.client.deliveryservice.delivery.presentation.dto.response.ListDeliveryRouteResponse;
 import com.winner.client.global.exception.BusinessException;
 import java.util.List;
@@ -31,11 +31,10 @@ public class DeliveryQueryServiceImpl implements DeliveryQueryService {
   @Override
   public ListDeliveryRouteResponse getDeliveryRoutes(UUID deliveryId) {
     validateDeliveryExists(deliveryId);
-    List<DeliveryRoute> routes = deliveryRouteRepository.findByDeliveryId(deliveryId);
+    List<DeliveryRoute> routes = deliveryRouteRepository.findAllByDeliveryId(deliveryId);
 
     return ListDeliveryRouteResponse.from(routes);
   }
-
 
   private void validateDeliveryExists(UUID deliveryId) {
     deliveryRepository.findById(deliveryId)
