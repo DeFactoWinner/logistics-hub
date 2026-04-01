50ac441905498a51437fb95a9f30000487abda71
yujin <yujin09219@naver.com>
2026-04-01 15:16:25 +0900

    chore : jwt 구현을 위한 설정

---
+++ global/src/main/java/com/winner/client/global/config/jwt/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.winner.client.global.config.jwt;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+public class JwtConfig {
+
+}
