9dba6dd802de20b87c138d81f3eee66f6750960a
sunsik17 <17sunsik@gmail.com>
2026-04-03 00:30:02 +0900

    chore: Remove unused HttpStatus import

--- delivery-service/src/main/java/com/winner/client/deliveryservice/deliverymanagercompany/presentation/internal/InternalDeliveryManagerCompanyController.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/deliverymanagercompany/presentation/internal/InternalDeliveryManagerCompanyController.java
@@ -9,7 +9,6 @@ import com.winner.client.global.response.ApiResponse;
 import com.winner.client.global.response.CommonSuccessCode;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
