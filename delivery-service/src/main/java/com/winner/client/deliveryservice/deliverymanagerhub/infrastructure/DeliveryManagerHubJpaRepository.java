8886e897a7c4d72cb5e5874c28a83380dade2bd7
sunsik17 <17sunsik@gmail.com>
2026-04-02 14:20:32 +0900

    feat: Implement DeliveryManagerHub registration process
    - Added `DeliveryManagerHubWriteService` with registration logic.
    - Introduced `InternalDeliveryManagerHubController` to handle internal API requests for DeliveryManagerHub.
    - Updated repository interfaces and JPA implementation to support additional methods (`existByUserId`, `findFirstAssignmentOrderOrderByDesc`, etc.).
    - Added capacity validation logic to restrict DeliveryManagerHub creation when over capacity.
    - Modified `DeliveryManagerHub` entity to include creation method with capacity check.

--- delivery-service/src/main/java/com/winner/client/deliveryservice/deliverymanagerhub/infrastructure/DeliveryManagerHubJpaRepository.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/deliverymanagerhub/infrastructure/DeliveryManagerHubJpaRepository.java
@@ -1,6 +1,9 @@
 package com.winner.client.deliveryservice.deliverymanagerhub.infrastructure;
 
+import com.winner.client.deliveryservice.deliverymanagerhub.domain.entity.DeliveryManagerHub;
 import com.winner.client.deliveryservice.deliverymanagerhub.domain.repository.DeliveryManagerHubRepository;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +13,23 @@ public class DeliveryManagerHubJpaRepository implements DeliveryManagerHubReposi
 
 	private final SpringDataDeliveryManagerHubRepository repository;
 
+	@Override
+	public DeliveryManagerHub save(DeliveryManagerHub deliveryManagerHub) {
+		return repository.save(deliveryManagerHub);
+	}
 
+	@Override
+	public boolean existByUserId(UUID userId) {
+		return repository.existsByUserId_Value(userId);
+	}
+
+	@Override
+	public Long count() {
+		return repository.count();
+	}
+
+	@Override
+	public Optional<DeliveryManagerHub> findFirstAssignmentOrderOrderByDesc() {
+		return repository.findFirstByOrderByAssignmentOrder_ValueDesc();
+	}
 }
