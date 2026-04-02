8445f888173845ead0086e062dafe945d3515408
yujin <yujin09219@naver.com>
2026-04-02 16:33:22 +0900

    feat : 의존성 주입을 위한 글로벌 패키지 스캔

--- user-service/src/main/java/com/winner/client/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/winner/client/userservice/UserServiceApplication.java
@@ -2,11 +2,22 @@ package com.winner.client.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.winner.client.userservice",
+    "com.winner.client.global"
+})
 public class UserServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
