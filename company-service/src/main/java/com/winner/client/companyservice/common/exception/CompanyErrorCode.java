b0276b51f5b03d929966480e15397a2650138018
rmkim4@gmail.com <rmkim4@gmail.com>
2026-04-03 01:52:23 +0900

    feat: add Company exception handler and update error codes

--- company-service/src/main/java/com/winner/client/companyservice/common/exception/CompanyErrorCode.java
+++ company-service/src/main/java/com/winner/client/companyservice/common/exception/CompanyErrorCode.java
@@ -1,27 +1,26 @@
 package com.winner.client.companyservice.common.exception;
 
-import com.winner.client.global.response.BaseCode;
+import com.winner.client.global.code.ErrorCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum CompanyErrorCode implements BaseCode {
+public enum CompanyErrorCode implements ErrorCode {
 
-  COMPANY_NOT_FOUND("COM_001", HttpStatus.NOT_FOUND, "존재하지 않는 업체입니다."),
-  DELETED_COMPANY("COM_002", HttpStatus.BAD_REQUEST, "이미 삭제된 업체입니다."),
-  HUB_NOT_FOUND("COM_003", HttpStatus.NOT_FOUND, "존재하지 않는 허브입니다."),
-  UNAUTHORIZED_ACCESS("COM_004", HttpStatus.FORBIDDEN, "해당 업체 정보에 접근 권한이 없습니다."),
-  ADDRESS_REQUIRED("COM_005", HttpStatus.BAD_REQUEST, "주소는 필수 입력값입니다."),
-  HUD_ID_REQUIRED("COM_006", HttpStatus.BAD_REQUEST,"허브 ID는 필수 입력값입니다."),
+  COMPANY_NOT_FOUND("ERROR_100", HttpStatus.NOT_FOUND, "존재하지 않는 업체입니다."),
+  DELETED_COMPANY("ERROR_101", HttpStatus.BAD_REQUEST, "이미 삭제된 업체입니다."),
+  HUB_NOT_FOUND("ERROR_102", HttpStatus.NOT_FOUND, "존재하지 않는 허브입니다."),
+  UNAUTHORIZED_ACCESS("ERROR_103", HttpStatus.FORBIDDEN, "해당 업체 정보에 접근 권한이 없습니다."),
+  ADDRESS_REQUIRED("ERROR_104", HttpStatus.BAD_REQUEST, "주소는 필수 입력값입니다."),
+  HUB_ID_REQUIRED("ERROR_105", HttpStatus.BAD_REQUEST,"허브 ID는 필수 입력값입니다."),
 
-  DUPLICATE_COMPANY_NAME("COM_07", HttpStatus.CONFLICT, "이미 사용 중인 업체명입니다."),
+  DUPLICATE_COMPANY("ERROR_106", HttpStatus.CONFLICT, "이미 사용 중인 업체입니다."),
 
-  INVALID_ADDRESS("COM_021", HttpStatus.BAD_REQUEST, "주소가 잘못되었습니다."),
-  INVALID_LATITUDE("COM_022", HttpStatus.BAD_REQUEST, "잘못된 위도값입니다."),
-  INVALID_LONGITUDE("COM_023", HttpStatus.BAD_REQUEST, "잘못된 경도값입니다."),
-  ADDRESS_API_ERROR("COM_024", HttpStatus.INTERNAL_SERVER_ERROR, "주소 조회 서비스와 통신 중 오류가 발생했습니다."),
-  ADDRESS_TRANSFORMATION_FAILED("COM_025", HttpStatus.BAD_REQUEST, "주소를 좌표로 변환하는 데 실패하였습니다.");
+  INVALID_ADDRESS("ERROR_107", HttpStatus.BAD_REQUEST, "주소가 잘못되었습니다."),
+  INVALID_LATITUDE("ERROR_108", HttpStatus.BAD_REQUEST, "잘못된 위도값입니다."),
+  INVALID_LONGITUDE("ERROR_109", HttpStatus.BAD_REQUEST, "잘못된 경도값입니다."),
+  ADDRESS_API_ERROR("ERROR_110", HttpStatus.INTERNAL_SERVER_ERROR, "주소 조회 서비스와 통신 중 오류가 발생했습니다."),
+  ADDRESS_TRANSFORMATION_FAILED("ERROR_111", HttpStatus.BAD_REQUEST, "주소를 좌표로 변환하는 데 실패하였습니다.");
 
   private final String code;
   private final HttpStatus status;
---
+++ company-service/src/main/java/com/winner/client/companyservice/common/exception/CompanyExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.winner.client.companyservice.common.exception;
+
+import com.winner.client.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CompanyExceptionHandler {
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+    return ResponseEntity
+        .status(HttpStatus.CONFLICT)
+        .body(ApiResponse.error(CompanyErrorCode.DUPLICATE_COMPANY));
+  }
+}
