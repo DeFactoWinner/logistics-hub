8a6ed4cc0b618d78089d6e11395a9bc3ac3aa1c4
yujin <yujin09219@naver.com>
2026-04-03 14:52:34 +0900

    fix : 의존성이 있는 경우에만 config 설정을 가져오도록 변경

--- global/src/main/java/com/winner/client/global/config/JpaAuditingConfig.java
+++ global/src/main/java/com/winner/client/global/config/JpaAuditingConfig.java
@@ -1,9 +1,12 @@
 package com.winner.client.global.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditingConfig {}
+@ConditionalOnClass(name = "jakarta.persistence.EntityManagerFactory")
+public class JpaAuditingConfig {
+
+}
