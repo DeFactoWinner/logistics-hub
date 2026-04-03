f4d2f0b45d226ee28f55e6ea8a61bfbdea1703cd
sunsik17 <17sunsik@gmail.com>
2026-04-04 00:36:49 +0900

    refactor: Move command and result classes to dto package for improved organization

--- delivery-service/src/main/java/com/winner/client/deliveryservice/deliverymanagercompany/application/dto/result/DeliveryManagerCompanyInfoResult.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/deliverymanagercompany/application/dto/result/DeliveryManagerCompanyInfoResult.java
@@ -1,4 +1,4 @@
-package com.winner.client.deliveryservice.deliverymanagercompany.application.result;
+package com.winner.client.deliveryservice.deliverymanagercompany.application.dto.result;
 
 import com.winner.client.deliveryservice.deliverymanagercompany.domain.entity.DeliveryManagerCompany;
 import java.time.LocalDateTime;
