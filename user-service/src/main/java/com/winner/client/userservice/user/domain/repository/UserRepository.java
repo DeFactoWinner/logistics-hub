d038cebf8acfdb9dd14b95cd546b2c533e08f518
yujin <yujin09219@naver.com>
2026-04-03 11:29:38 +0900

    feat : 레포 함수 추가

--- user-service/src/main/java/com/winner/client/userservice/user/domain/repository/UserRepository.java
+++ user-service/src/main/java/com/winner/client/userservice/user/domain/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.winner.client.userservice.user.domain.repository;
 
 import com.winner.client.userservice.user.domain.entity.User;
+import java.util.Optional;
 
 public interface UserRepository {
 
   User save(User user);
 
-  boolean findByUsernameAndDeletedAtNull(String username);
+  boolean existsByUsernameAndDeletedAtNull(String username);
+
+  Optional<User> findByUsernameAndDeletedAtNull(String username);
 }
