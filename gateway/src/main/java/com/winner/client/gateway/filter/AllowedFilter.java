d7a727369da3901e968055e8e3ded096d65c29eb
yujin <yujin09219@naver.com>
2026-04-03 22:26:57 +0900

    feat: 게이트 웨이 필터 적용

---
+++ gateway/src/main/java/com/winner/client/gateway/filter/AllowedFilter.java
@@ -0,0 +1,36 @@
+package com.winner.client.gateway.filter;
+
+import com.winner.client.global.variable.AuthProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AllowedFilter implements GlobalFilter, Ordered {
+
+  private final AuthProperties authProperties;
+  private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    String path = exchange.getRequest().getURI().getPath();
+    boolean isWhiteList = authProperties.getWhiteList().stream()
+        .anyMatch(p -> pathMatcher.match(p.trim(), path));
+
+    if (isWhiteList) {
+      return Mono.empty();
+    }
+    return chain.filter(exchange);
+  }
+
+  @Override
+  public int getOrder() {
+    return 0;
+  }
+}
