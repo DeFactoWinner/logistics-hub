08ec9fff3173c00fd6b8a285097d386e80a91722
hyew0nn <hyewon-0607@naver.com>
2026-04-03 15:42:23 +0900

    feat: 허브 루트 경로를 호출하는 FeignClient 추가

---
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/infrastructure/client/HubClient.java
@@ -0,0 +1,13 @@
+package com.winner.client.deliveryservice.delivery.infrastructure.client;
+
+import com.winner.client.deliveryservice.delivery.infrastructure.client.dto.HubRouteResponse;
+import java.util.UUID;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="hub-service")
+public interface HubClient {
+  @GetMapping("/internal/v1/hub-routes")
+  HubRouteResponse getHubRoutes(@RequestParam UUID fromHubId, @RequestParam UUID toHubId);
+}
