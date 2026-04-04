a72c67dbaeddc959814794cf1f431bc7d1f3e5c8
githyj-jang <hoo9268@gmail.com>
2026-04-05 01:04:53 +0900

    feat: add InternalOrderController for handling internal order requests

---
+++ order-service/src/main/java/com/winner/orderservice/order/presentation/controller/InternalOrderController.java
@@ -0,0 +1,12 @@
+package com.winner.orderservice.order.presentation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/internal/v1/orders")
+public class InternalOrderController {
+
+}
