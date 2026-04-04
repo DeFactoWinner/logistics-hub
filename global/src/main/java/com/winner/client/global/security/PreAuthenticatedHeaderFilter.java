1056364436c5e261d57dab70ca339359cdff7d43
yujin <yujin09219@naver.com>
2026-04-05 00:39:50 +0900

    refactor: 로그 설정 및 코드 정리

--- global/src/main/java/com/winner/client/global/security/PreAuthenticatedHeaderFilter.java
+++ global/src/main/java/com/winner/client/global/security/PreAuthenticatedHeaderFilter.java
@@ -21,6 +21,19 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 public class PreAuthenticatedHeaderFilter extends OncePerRequestFilter {
 
+  private static UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(
+      UUID userId, String userRole, UUID referenceId) {
+    CustomUserPrincipal customUserPrincipal =
+        new CustomUserPrincipal(userId, userRole, referenceId);
+
+    List<SimpleGrantedAuthority> authorities = Collections.emptyList();
+    if (userRole != null && !userRole.isEmpty()) {
+      String roleWithPrefix = "ROLE_" + userRole;
+      authorities = Collections.singletonList(new SimpleGrantedAuthority(roleWithPrefix));
+    }
+    return new UsernamePasswordAuthenticationToken(customUserPrincipal, null, authorities);
+  }
+
   @Override
   protected void doFilterInternal(HttpServletRequest request,
       HttpServletResponse response,
@@ -28,7 +41,14 @@ public class PreAuthenticatedHeaderFilter extends OncePerRequestFilter {
 
     String userIdStr = request.getHeader("X-User-Id");
     String userRole = request.getHeader("X-User-Role");
-    Object referenceIdObj = request.getHeader("X-User-ReferenceId");
+    String referenceIdStr = request.getHeader("X-User-ReferenceId");
+    String skipAuth = request.getHeader("X-Auth-Skip");
+
+    log.info("[Security 필터] 요청 감지 - Path: {}", request);
+    if ("true".equalsIgnoreCase(skipAuth)) {
+      filterChain.doFilter(request, response);
+      return;
+    }
 
     if (userIdStr == null || userIdStr.isEmpty()) {
       throw new BusinessException(CommonErrorCode.UNAUTHORIZED);
@@ -36,24 +56,18 @@ public class PreAuthenticatedHeaderFilter extends OncePerRequestFilter {
 
     try {
       UUID userId = UUID.fromString(userIdStr);
-      UUID referenceId;
+      UUID referenceId = null;
 
-      try {
-        referenceId = UUID.fromString(referenceIdObj.toString());
-      } catch (IllegalArgumentException e) {
-        referenceId = null;
+      if (referenceIdStr != null && !referenceIdStr.isEmpty() && !"null".equals(referenceIdStr)) {
+        try {
+          referenceId = UUID.fromString(referenceIdStr);
+        } catch (IllegalArgumentException ignored) {
+        }
       }
-      CustomUserPrincipal customUserPrincipal =
-          new CustomUserPrincipal(userId, userRole, referenceId);
-
-      List<SimpleGrantedAuthority> authorities = Collections.emptyList();
-      if (userRole != null && !userRole.isEmpty()) {
-        authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + userRole));
-      }
-      UsernamePasswordAuthenticationToken authentication =
-          new UsernamePasswordAuthenticationToken(customUserPrincipal, null, authorities);
+      UsernamePasswordAuthenticationToken authentication = getUsernamePasswordAuthenticationToken(
+          userId, userRole, referenceId);
       SecurityContextHolder.getContext().setAuthentication(authentication);
-    } catch (IllegalArgumentException e) {
+    } catch (Exception e) {
       throw new BusinessException(CommonErrorCode.UNAUTHORIZED);
     }
     filterChain.doFilter(request, response);
