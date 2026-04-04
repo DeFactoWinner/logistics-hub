249a7c7b93b1abbf6e8b7bdc2c8f2ec3d860b4c1
rmkim4@gmail.com <rmkim4@gmail.com>
2026-04-05 04:39:29 +0900

    feat: dto 추가 , vo 수정

--- product-service/src/main/java/com/winner/client/productservice/product/application/service/dto/result/ProductDetailResult.java
+++ product-service/src/main/java/com/winner/client/productservice/product/application/service/dto/result/ProductDetailResult.java
@@ -2,10 +2,12 @@ package com.winner.client.productservice.product.application.service.dto.result;
 
 import com.winner.client.productservice.product.domain.entity.Product;
 import com.winner.client.productservice.product.domain.vo.StatusEnum;
+import com.winner.client.productservice.stock.domain.entity.Stock;
+import com.winner.client.productservice.stock.domain.vo.Quantity;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record ProductResult(
+public record ProductDetailResult(
     UUID productId,
     String productName,
     String description,
@@ -13,20 +15,26 @@ public record ProductResult(
     UUID companyId,
     StatusEnum statusEnum,
 
+    UUID stockId,
+    int quantity,
+
     LocalDateTime createdAt,
     LocalDateTime updatedAt
 ) {
 
-  public static ProductResult from(Product product){
-    return new ProductResult(
+  public static ProductDetailResult of(Product product, Stock stock){
+    return new ProductDetailResult(
         product.getId(),
         product.getName(),
         product.getDescription(),
         product.getHubId().getHubId(),
         product.getCompanyId().getCompanyId(),
         product.getStatusEnum(),
+        stock.getId(),
+        stock.getQuantity().getQuantity(),
         product.getCreatedAt(),
         product.getUpdatedAt()
     );
   }
+
 }
---
+++ product-service/src/main/java/com/winner/client/productservice/product/presentation/dto/response/ListProductResponse.java
@@ -0,0 +1,14 @@
+package com.winner.client.productservice.product.presentation.dto.response;
+
+import com.winner.client.productservice.product.application.service.dto.result.ProductDetailResult;
+import org.springframework.data.domain.Page;
+
+public record ListProductResponse(
+    Page<ProductDetailResult> list,
+    Long totalCount
+) {
+
+  public static ListProductResponse of(Page<ProductDetailResult> list, Long totalCount) {
+    return new ListProductResponse(list, totalCount);
+  }
+}
