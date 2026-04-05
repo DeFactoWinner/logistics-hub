e54f4c537787242f51f421c37f0d135af4c1837a
yujin <yujin09219@naver.com>
2026-04-06 03:05:09 +0900

    refactor : 폴더 구조 리팩토링

--- user-service/src/main/java/com/winner/client/userservice/user/application/service/UserQueryService.java
+++ user-service/src/main/java/com/winner/client/userservice/user/application/service/UserQueryService.java
@@ -1,6 +1,6 @@
 package com.winner.client.userservice.user.application.service;
 
-import com.winner.client.userservice.user.application.result.UserDetailResult;
+import com.winner.client.userservice.user.application.dto.result.UserDetailResult;
 import java.util.UUID;
 
 public interface UserQueryService {
