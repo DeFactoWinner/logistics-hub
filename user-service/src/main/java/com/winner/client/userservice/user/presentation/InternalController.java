a712c5bfb75e91ec05c9684496ea1a2d9e71f45e
yujin <yujin09219@naver.com>
2026-04-06 00:05:45 +0900

    feat : 유저 조회 엔드포인트 연결

---
+++ user-service/src/main/java/com/winner/client/userservice/user/presentation/InternalController.java
@@ -0,0 +1,39 @@
+package com.winner.client.userservice.user.presentation;
+
+import com.winner.client.global.response.ApiResponse;
+import com.winner.client.global.response.CommonSuccessCode;
+import com.winner.client.userservice.user.application.service.UserQueryService;
+import com.winner.client.userservice.user.presentation.response.UserDetailResponse;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/v1/users")
+@RequiredArgsConstructor
+public class InternalController {
+
+  private final UserQueryService userQueryService;
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<ApiResponse<UserDetailResponse>> getUserDetailsForManager(
+      @PathVariable(name = "userId") UUID userId) {
+    return ResponseEntity
+        .status(
+            CommonSuccessCode.OK.getStatus()
+        )
+        .body(
+            ApiResponse.success(
+                CommonSuccessCode.OK,
+                "해당 유저를 조회하였습니다.",
+                UserDetailResponse.from(
+                    userQueryService.getUserDetail(userId)
+                )
+            )
+        );
+  }
+}
