6e12cd238a3692017b41279a1b2327277c14b960
yujin <yujin09219@naver.com>
2026-04-05 16:26:23 +0900

    feat : 토큰 재발급 함수 추가 및 공통 로직 분리

--- user-service/src/main/java/com/winner/client/userservice/user/application/service/AuthCommandService.java
+++ user-service/src/main/java/com/winner/client/userservice/user/application/service/AuthCommandService.java
@@ -1,13 +1,16 @@
 package com.winner.client.userservice.user.application.service;
 
 import com.winner.client.userservice.user.application.command.LoginCommand;
+import com.winner.client.userservice.user.application.command.RefreshTokenCommand;
 import com.winner.client.userservice.user.application.command.SignupCommand;
-import com.winner.client.userservice.user.application.result.LoginResult;
 import com.winner.client.userservice.user.application.result.SignupResult;
+import com.winner.client.userservice.user.application.result.TokenResult;
 
 public interface AuthCommandService {
 
   SignupResult signup(SignupCommand command);
 
-  LoginResult login(LoginCommand command);
+  TokenResult login(LoginCommand command);
+
+  TokenResult refreshToken(RefreshTokenCommand command);
 }
