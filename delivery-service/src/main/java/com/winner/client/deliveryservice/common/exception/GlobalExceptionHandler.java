8997d178d032c16fe22bb254acede27951bdbd46
hyew0nn <hyewon-0607@naver.com>
2026-04-06 14:34:42 +0900

    feat: 메소드 및 검증 에러 핸들러 추가

--- delivery-service/src/main/java/com/winner/client/deliveryservice/common/exception/GlobalExceptionHandler.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/common/exception/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.winner.client.deliveryservice.common.exception;
 import com.winner.client.global.exception.BusinessException;
 import com.winner.client.global.exception.CommonErrorCode;
 import com.winner.client.global.response.ApiResponse;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +25,32 @@ public class GlobalExceptionHandler {
         .body(ApiResponse.error(e.getErrorCode()));
   }
 
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public ResponseEntity<ApiResponse<Void>> handleHttpRequestMethodNotSupportedException(
+      HttpRequestMethodNotSupportedException e) {
+    log.warn("HttpRequestMethodNotSupportedException: {}", e.getMessage());
+    return ResponseEntity
+        .status(HttpStatus.METHOD_NOT_ALLOWED)
+        .body(ApiResponse.error(CommonErrorCode.METHOD_NOT_ALLOWED));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  protected ResponseEntity<ApiResponse<Map<String, String>>> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException ex
+  ) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getGlobalErrors().forEach(error ->
+        errors.put(error.getObjectName(), error.getDefaultMessage())
+    );
+    ex.getBindingResult().getFieldErrors().forEach(error ->
+        errors.put(error.getField(), error.getDefaultMessage())
+    );
+
+    log.warn("MethodArgumentNotValidException: {}", errors);
+
+    CommonErrorCode code = CommonErrorCode.INVALID_INPUT;
+    return ResponseEntity.status(code.getStatus()).body(ApiResponse.error(code, errors));
+  }
 
   @ExceptionHandler(Exception.class)
   protected ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
