8b4588ee7fa111c47a1125674cd1c1a83e31bcec
hyew0nn <hyewon-0607@naver.com>
2026-04-07 01:51:46 +0900

    feat: @TransactionalEventListener 추가

--- delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/application/service/DeliveryAssignmentService.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/application/service/DeliveryAssignmentService.java
@@ -1,10 +1,12 @@
 package com.winner.client.deliveryservice.delivery.application.service;
 
 import com.winner.client.deliveryservice.delivery.domain.entity.Delivery;
+import com.winner.client.deliveryservice.delivery.domain.entity.DeliveryRoute;
+import java.util.List;
 import java.util.UUID;
 
 public interface DeliveryAssignmentService {
   void assignCompanyDeliveryManager(Delivery delivery);
-  void assignHubDeliveryManager(Delivery delivery);
+  void assignHubDeliveryManager(Delivery delivery, List<DeliveryRoute> routes);
   void retryHubDeliveryManager(UUID deliveryId);
 }
--- delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/infrastructure/message/DeliverySpringEventListener.java
+++ delivery-service/src/main/java/com/winner/client/deliveryservice/delivery/infrastructure/message/DeliverySpringEventListener.java
@@ -15,8 +15,11 @@ import com.winner.client.deliveryservice.delivery.application.dto.command.HubDel
 import com.winner.client.deliveryservice.delivery.application.service.DeliveryMessageUsecase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -25,37 +28,43 @@ public class DeliverySpringEventListener {
 
 	private final DeliveryMessageUsecase usecase;
 
-	@EventListener
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void handleHubAssignSuccess(AssignHubDeliveryManagerSuccessEvent event) {
 		log.info("Hub 배정 성공 이벤트 수신: {}", event.deliveryId());
 		usecase.completeHubDeliveryManagerAssign(DeliveryRouteAssignCompleteCommand.from(event));
 	}
 
-	@EventListener
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void handleHubAssignFail(AssignHubDeliveryManagerFailEvent event) {
 		log.error("Hub 배정 실패 이벤트 수신: {}", event.message());
 		usecase.retryHubDeliveryManagerAssign(HubDeliveryManagerAssignFailCommand.from(event));
 	}
 
-	@EventListener
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void handleHubDeliveryComplete(DeliveryCompleteEvent event) {
 		log.info("Hub 배달 완료 이벤트 수신: {}", event.deliveryId());
 		usecase.completeDeliveryRoute(CompleteDeliveryRouteCommand.from(event));
 	}
 
-	@EventListener
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void handleCompanyAssignSuccess(AssignCompanyDeliveryManagerSuccessEvent event) {
 		log.info("업체 배정 성공 이벤트 수신: {}", event.deliveryId());
 		usecase.completeCompanyDeliveryManagerAssign(DeliveryAssignCompleteCommand.from(event));
 	}
 
-	@EventListener
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void handleCompanyAssignFail(AssignCompanyDeliveryManagerFailEvent event) {
 		log.error("업체 배정 실패 이벤트 수신 {}", event.message());
 		usecase.retryCompanyDeliveryManagerAssign(CompanyDeliveryManagerAssignFailCommand.from(event));
 	}
 
-	@EventListener
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void handleCompanyFinalComplete(DeliveryFinalCompleteEvent event) {
 		log.info("최종 배달 완료 이벤트 수신: {}", event.deliveryId());
 		usecase.completeDelivery(CompleteDeliveryCommand.from(event));
